00f50af215b3161047f5f1cf105e34209db34550
IAV <IlyaVorWork2408@gmail.com>
2023-04-26 09:49:28 +0700

    Переезд

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,16 +1,27 @@
 import { useUser } from "@clerk/nextjs";
 import { NextComponentType } from "next";
 import Image from "next/image";
+import { useState } from "react";
+import { api } from "~/utils/api";
 
 const CreatePost: NextComponentType = () => {
     const {user} = useUser();
+    console.log(user)
+    const [content, setContent] = useState("");
 
     if (!user) return null
 
     return (
         <div className="flex gap-3 w-full">
-            <Image src={user.profileImageUrl} alt="userAvatar" className="w-16 h-16 rounded-full"/>
-            <input placeholder="Type some emojis!" className="bg-transparent grow outline-none"/>
+            <img src={user.profileImageUrl} alt="userAvatar" className="w-16 h-16 rounded-full"/>
+            <input 
+                type="text"
+                placeholder="Type some emojis!" 
+                className="bg-transparent grow outline-none" 
+                onInput={(e) => setContent(e.currentTarget.value)}
+                value={content}
+            />
+            <button /*onClick={() => mutate({content, authorId: user.id})}*/ className="bg-white text-black w-32 h-8">Send</button>
         </div>
     )
 }
